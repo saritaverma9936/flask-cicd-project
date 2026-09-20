42e734eb534e0a04dfc529c11edf3d04b8241b73
Sarita Verma <saritaverma9936@gmail.com>
2026-09-20 15:56:11 +0530

    Update Flask application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD Project!"
+    return "Hello from Flask CI/CD Project - Deployed Automatically!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
