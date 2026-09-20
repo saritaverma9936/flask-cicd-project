9081c88598eaad9858ae62c30fab8e9845e361f7
Sarita Verma <saritaverma9936@gmail.com>
2026-09-20 16:00:09 +0530

    Update test for deployed message

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    
+
     assert response.status_code == 200
-    assert b"Hello from Flask CI/CD Project!" in response.data
+    assert b"Hello from Flask CI/CD Project - Deployed Automatically!" in response.data
